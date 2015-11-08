540ab87b8f9197a54faf194d3f6edb8d1ca4bbf4
dargaCode <darga.code@gmail.com>
2015-11-08 14:12:58 -0500

    place main at bottom and remove prototypes per reddit

--- concat.c
+++ concat.c
@@ -3,28 +3,6 @@
 #include <string.h>
 #include <cs50.h>
 
-char * concatenate(char *, char *);
-
-int main(void)
-{
-    printf("\nEnter String 1: ");
-    char * input_a = GetString();
-    printf("Enter String 2: ");
-    char * input_b = GetString();
-    printf("\nConcatenate \"%s\" and \"%s\":\n", input_a, input_b);
-
-    char * result = concatenate(input_a, input_b);
-
-    printf("    Result: %s\n", result);
-    printf("    Length: %i\n\n", strlen(result));
-
-    // free memory from concatenate function
-    free(result);
-
-    // success
-    return 0;
-}
-
 char * concatenate(char * str_a, char * str_b)
 {
     int len_a = strlen(str_a);
@@ -52,4 +30,24 @@ char * concatenate(char * str_a, char * str_b)
     result[new_len] = '\0';
 
     return result;
+}
+
+int main(void)
+{
+    printf("\nEnter String 1: ");
+    char * input_a = GetString();
+    printf("Enter String 2: ");
+    char * input_b = GetString();
+    printf("\nConcatenate \"%s\" and \"%s\":\n", input_a, input_b);
+
+    char * result = concatenate(input_a, input_b);
+
+    printf("    Result: %s\n", result);
+    printf("    Length: %i\n\n", strlen(result));
+
+    // free memory from concatenate function
+    free(result);
+
+    // success
+    return 0;
 }
